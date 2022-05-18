f9550ec4f903346300403d00922c1b967589a76f
si-shakil <sishakil850@gmail.com>
2022-05-18 19:11:04 +0600

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home/Home";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
