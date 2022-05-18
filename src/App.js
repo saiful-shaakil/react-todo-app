abe31d3b4d9b41a97a588d0e227cafc02aae8fea
si-shakil <sishakil850@gmail.com>
2022-05-18 19:35:54 +0600

    navbar and firebase and others tech added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home/Home";
+import Navbar from "./Components/Home/Navbar";
+import Login from "./Components/User/Login";
 
 function App() {
   return (
     <div>
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/todolist" element={<Home />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </div>
   );
